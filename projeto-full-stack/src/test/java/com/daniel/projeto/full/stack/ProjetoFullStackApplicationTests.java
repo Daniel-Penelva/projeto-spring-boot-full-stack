3cf03dfcea8e9385b23ba0a20a981bd5e28801f5
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-11 23:37:36 -0300

    Criando projeto Maven API Rest

---
+++ projeto-full-stack/src/test/java/com/daniel/projeto/full/stack/ProjetoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.projeto.full.stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
