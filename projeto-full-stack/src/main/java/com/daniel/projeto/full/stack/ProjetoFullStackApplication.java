a7d04ab3317f7d8575941bc30fa4e3817b77ab8f
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-12 20:55:41 -0300

    Criando ProdutoRepository e Produto e associação muitos para muitos

--- projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
+++ projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.projeto.full.stack.domain.Categoria;
+import com.daniel.projeto.full.stack.domain.Produto;
 import com.daniel.projeto.full.stack.repositories.CategoriaRepository;
+import com.daniel.projeto.full.stack.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoFullStackApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFullStackApplication.class, args);
@@ -24,8 +29,21 @@ public class ProjetoFullStackApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		// Instanciando os objetos da relação Produto e Categoria
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
