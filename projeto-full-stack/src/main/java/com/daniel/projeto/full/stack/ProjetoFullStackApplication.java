d5d06799723a459778332a995100cf65c44ce97a
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-12 19:52:40 -0300

    Criando operação de instanciação

--- projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
+++ projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
@@ -1,13 +1,31 @@
 package com.daniel.projeto.full.stack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.daniel.projeto.full.stack.domain.Categoria;
+import com.daniel.projeto.full.stack.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoFullStackApplication {
+public class ProjetoFullStackApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFullStackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
