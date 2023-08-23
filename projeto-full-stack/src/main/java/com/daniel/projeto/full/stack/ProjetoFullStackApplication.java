43b6662a7f07d4a9192429f03d9aa3dbb44b5295
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-23 17:50:35 -0300

    Criando o ItemPedidoRepository e instanciando os objetos item pedido

--- projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
+++ projeto-full-stack/src/main/java/com/daniel/projeto/full/stack/ProjetoFullStackApplication.java
@@ -13,6 +13,7 @@ import com.daniel.projeto.full.stack.domain.Cidade;
 import com.daniel.projeto.full.stack.domain.Cliente;
 import com.daniel.projeto.full.stack.domain.Endereco;
 import com.daniel.projeto.full.stack.domain.Estado;
+import com.daniel.projeto.full.stack.domain.ItemPedido;
 import com.daniel.projeto.full.stack.domain.Pagamento;
 import com.daniel.projeto.full.stack.domain.PagamentoComBoleto;
 import com.daniel.projeto.full.stack.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.daniel.projeto.full.stack.repositories.CidadeRepository;
 import com.daniel.projeto.full.stack.repositories.ClienteRepository;
 import com.daniel.projeto.full.stack.repositories.EnderecoRepository;
 import com.daniel.projeto.full.stack.repositories.EstadoRepository;
+import com.daniel.projeto.full.stack.repositories.ItemPedidoRepository;
 import com.daniel.projeto.full.stack.repositories.PagamentoRepository;
 import com.daniel.projeto.full.stack.repositories.PedidoRepository;
 import com.daniel.projeto.full.stack.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class ProjetoFullStackApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFullStackApplication.class, args);
@@ -125,6 +130,21 @@ public class ProjetoFullStackApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		/* --------------------------------------------------------- */
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
